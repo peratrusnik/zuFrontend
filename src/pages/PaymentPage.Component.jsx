8ab0db2f4a97b3a6a75db963800a14c5fe0cef39
Pera <peratrusnik@gmail.com>
2023-04-04 18:08:25 +0200

    upd

--- src/pages/PaymentPage.Component.jsx
+++ src/pages/PaymentPage.Component.jsx
@@ -6,14 +6,14 @@ import {toast} from "react-toastify";
 import {CardElement, Elements} from "@stripe/react-stripe-js";
 import PaymentElementsComponent from "../components/payment/PaymentElements.Component";
 // public key
-const pk = 'pk_test_51LE86WICBi42q51NTvYro1xsDdciS1c7P588igQVzS0sAF64k9cDdJxlyxufgIeSdQcI7v83lPO4MPrzqGtdG3yV00n15bVGRe'
+const pk = 'pk_test_51MtBuXEdr6WugQ8Xxoo4cIuemWTiLd7acHptY6wHPUVRmcv3j9NzzI06axbfJYMnUUvQakPQlfSupDsC5tNXezMT00gO5vWEvB'
 const stripeObj = loadStripe(pk)
 const PaymentPageComponent = () => {
     // client key
     const [ck, setCk] = useState('')
     const totalPrice = useSelector(state => state.cartStore.totalPrice)
     // todo: add logic for switching curr in nav bar component
-    const cur = 'usd'
+    const cur = "eur"
     const stripeOption = {clientSecret: ck}
 
     useEffect(() => {

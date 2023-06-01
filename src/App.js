d36b1b97c2ee2406d97f3118e7915e8a98dfaba7
Pera <peratrusnik@gmail.com>
2023-06-01 15:06:10 +0200

    upd

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ScrollToTopComponent from './components/ScrollToTop/ScrollToTop.Component';
 
-const serverBackend = "https://slate-gray-harp-seal-gown.cyclic.app/api"
+const serverBackend = "https://zurea-shop.onrender.com/api"
 const localhostAddress = "http://localhost:5050/api"
 
 if(process.env.NODE_ENV ==="development"){
